881e12d7abbd4819456abfdef7b05c14962aa47e
Dima Sidenko <sidenkodima2202@gmail.com>
2023-10-14 09:21:57 +0300

    add dark mode

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-// // 1. Проверка темной темы на уровне системных настроек
-// if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
-//     btnDarkMode.classList.add("dark-mode-btn--active");
-// 	document.body.classList.add("dark");
-// }
+// 1. Проверка темной темы на уровне системных настроек
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+	document.body.classList.add("dark");
+}
 
-// // 2. Проверка темной темы в localStorage
-// if (localStorage.getItem('darkMode') === 'dark') {
-//     btnDarkMode.classList.add("dark-mode-btn--active");
-//     document.body.classList.add("dark");
-// } else if (localStorage.getItem("darkMode") === "light") {
-//     btnDarkMode.classList.remove("dark-mode-btn--active");
-//     document.body.classList.remove("dark");
-// }
+// 2. Проверка темной темы в localStorage
+if (localStorage.getItem('darkMode') === 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
 
-// // Если меняются системные настройки, меняем тему
-// window
-//     .matchMedia("(prefers-color-scheme: dark)")
-//     .addEventListener("change", (event) => {
-//         const newColorScheme = event.matches ? "dark" : "light";
+// Если меняются системные настройки, меняем тему
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
 
-//         if (newColorScheme === "dark") {
-// 			btnDarkMode.classList.add("dark-mode-btn--active");
-// 			document.body.classList.add("dark");
-// 			localStorage.setItem("darkMode", "dark");
-// 		} else {
-// 			btnDarkMode.classList.remove("dark-mode-btn--active");
-// 			document.body.classList.remove("dark");
-// 			localStorage.setItem("darkMode", "light");
-// 		}
-//     });
+        if (newColorScheme === "dark") {
+			btnDarkMode.classList.add("dark-mode-btn--active");
+			document.body.classList.add("dark");
+			localStorage.setItem("darkMode", "dark");
+		} else {
+			btnDarkMode.classList.remove("dark-mode-btn--active");
+			document.body.classList.remove("dark");
+			localStorage.setItem("darkMode", "light");
+		}
+    });
 
 // Включение ночного режима по кнопке
 btnDarkMode.onclick = function () {
